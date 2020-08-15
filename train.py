98cc6edfd7aa41ddea101c8c9a6fe203011d9392
ykchua1 <ykchua1@gmail.com>
2020-08-15 09:03:18 +0000

    Updated training script for CUDA usage

--- train.py
+++ train.py
@@ -8,10 +8,12 @@ import os
 from darknet import Darknet
 from random import shuffle
 
+CUDA = torch.cuda.is_available()
+
 # set up the neural network
 print("Loading network...")
-cfgfile = "/home/jovyan/work/YOLO_v3_tutorial_from_scratch/cfg/yolov3.cfg"
-weightsfile = "/home/jovyan/work/YOLO_v3_tutorial_from_scratch/yolov3.weights"
+cfgfile = os.path.abspath("cfg/yolov3.cfg") # "/home/jovyan/work/YOLO_v3_tutorial_from_scratch/cfg/yolov3.cfg"
+weightsfile = os.path.abspath("yolov3.weights") # "/home/jovyan/work/YOLO_v3_tutorial_from_scratch/yolov3.weights"
 model = Darknet(cfgfile)
 model.load_weights(weightsfile)
 print("Network successfully loaded")
@@ -24,8 +26,10 @@ for i in det_layers:
     model.blocks[i+1]["classes"] = 4
 print("Layers have been swapped out")    
 
+if CUDA:
+    model.to(torch.device("cuda"))
+
 # training loop
-CUDA = torch.cuda.is_available()
 lambda_coord = 0.01
 lambda_noobj = 0.1
 batch_size = 2
@@ -36,6 +40,8 @@ lr = 0.001
 import torch.optim as optim
 optimizer = optim.Adam(model.parameters(), lr=lr)
 mse_loss = nn.MSELoss(reduction='sum')
+if CUDA:
+    mse_loss.to(torch.device("cuda"))
 
 imlist = os.listdir("./data/scattered_coins/")
 imlist = list(filter(lambda x: x.split('.')[-1] == "jpg", imlist))
@@ -52,6 +58,8 @@ for epoch in range(epochs):
     num_batches = len(imlist) // batch_size + leftover
     im_batches = list(map(prep_image, loaded_ims, [416 for x in range(len(loaded_ims))]))
     im_batches = [torch.cat((im_batches[i*batch_size : min((i+1)*batch_size, len(im_batches))])) for i in range(num_batches)]
+    if CUDA:
+        im_batches = [im_batch.to(torch.device("cuda")) for im_batch in im_batches]
     
     for i, batch in enumerate(im_batches):
         fp_list = [imlist[i*batch_size + x][:-4]+".txt" for x in range(batch.shape[0])]
@@ -66,10 +74,17 @@ for epoch in range(epochs):
         mask2 = create_training_mask_2(inp, fp_list, iou_thresh=0.5)
 
         targ = create_groundtruth(inp, fp_list)
+        if CUDA:
+            mask1 = mask1.to(torch.device("cuda"))
+            mask2 = mask2.to(torch.device("cuda"))
+            targ = targ.to(torch.device("cuda"))
         
         sq_err_loss = lambda_coord * mse_loss((mask1*inp)[:,:,:4], 416*targ[:,:,:4]) # multiplied by 416 to scale up w/ model output
         cross_entr_loss = cross_entropy(mask1*inp, targ)
-        cross_entr_loss_noobj = lambda_noobj * cross_entropy(mask2*inp, torch.zeros(inp.shape))
+        zero_tensor = torch.zeros(inp.shape)
+        if CUDA:
+            zero_tensor = zero_tensor.to(torch.device("cuda"))
+        cross_entr_loss_noobj = lambda_noobj * cross_entropy(mask2*inp, zero_tensor)
         
         loss = sq_err_loss + cross_entr_loss + cross_entr_loss_noobj
         loss.backward()
