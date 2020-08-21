d49c4c6285d88908dce31c28b805c45809addde2
ykchua1 <ykchua1@gmail.com>
2020-08-21 11:07:55 +0000

    Added modified cfg files, updated get-anch code

--- get_k_means_anch.py
+++ get_k_means_anch.py
@@ -19,3 +19,17 @@ for file in os.listdir("./data/scattered_coins/"):
             wh_tuple_list.append((line.split()[3], line.split()[4]))
             
 X = np.array(wh_tuple_list)
+kmeans = KMeans(n_clusters=9, random_state=0).fit(X)
+
+# sort by area
+clust_centers = kmeans.cluster_centers_
+areas = [(i, x[0] * x[1]) for i, x in enumerate(clust_centers)]
+areas.sort(key=lambda x: x[1])
+
+area_ranked_ind = [x[0] for x in areas]
+clust_centers = clust_centers[area_ranked_ind]
+
+clust_centers *= 416
+clust_centers = np.rint(clust_centers)
+
+print(clust_centers)
