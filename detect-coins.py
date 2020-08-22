d49bd7f87fd58d7a3ae810864680c9387a70f483
ykchua1 <ykchua1@gmail.com>
2020-08-22 11:22:10 +0000

    Changed detect-coins.py such that same class have same color

--- detect-coins.py
+++ detect-coins.py
@@ -201,7 +201,8 @@ def write(x, results): #
     c2 = tuple(x[3:5].int())
     img = results[int(x[0])]
     cls = int(x[-1])
-    color = random.choice(colors)
+    # color = random.choice(colors)
+    color = colors[cls]
     label = "{0}".format(classes[cls])
     cv2.rectangle(img, c1, c2,color, 1)
     t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 1 , 1)[0]
